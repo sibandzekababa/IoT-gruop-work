65229438c9ddb12c7b3d3ccfc28e258417508cf2
sibandzekababa <sibandzekababa.gmail.com>
2026-07-24 05:00:57 +0300

    Added secrets code

--- src/main.cpp
+++ src/main.cpp
@@ -2,26 +2,27 @@
 #include <WiFi.h>
 #include <WiFiClientSecure.h>
 #include <PubSubClient.h>
+#include "secrets.h"
+
+const char *ssid = SECRET_SSID;
+const char *password = SECRET_PASSWORD;
+const char *broker = SECRET_BROKER;
+const int port = 8883;
+const char *mqtt_user = SECRET_MQTT_USER;
+const char *mqtt_password = SECRET_MQTT_PASSWORD;
 
 WiFiClientSecure KB;
 PubSubClient mqttClient(KB);
 
-const char* SSID = "Clasrooms";
-const char* Pass = "Chix@Akira";
-const char* BrokerURL = "61917c0a47134645af2b8a974c7bcc8e.s1.eu.hivemq.cloud";
-const int port = 8883;
-const char* user_mqtt = "Kababa";
-const char* mqtt_pass = "Kababa wa Bongwe";
-
 void connectMQTT() {
   KB.setInsecure();
-  mqttClient.setServer(BrokerURL, port);
+  mqttClient.setServer(broker, port);
 
   while (!mqttClient.connected()) {
     String clientId = "esp32-" + String((uint32_t)ESP.getEfuseMac(), HEX);
     Serial.printf("MQTT: connecting as %s ", clientId.c_str());
 
-    if (mqttClient.connect(clientId.c_str(), user_mqtt, mqtt_pass)) {
+    if (mqttClient.connect(clientId.c_str(), mqtt_user, mqtt_password)) {
       Serial.println("-> connected.");
     } else {
       Serial.printf("-> failed (rc=%d). Retrying in 2s\n", mqttClient.state());
@@ -34,7 +35,7 @@ void setup() {
   Serial.begin(115200);
 
   WiFi.mode(WIFI_STA);
-  WiFi.begin(SSID, Pass);
+  WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
---
+++ src/secrets.h
@@ -0,0 +1,10 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+#define SECRET_SSID "Clasrooms"
+#define SECRET_PASSWORD "Chix@Akira"
+#define SECRET_BROKER "61917c0a47134645af2b8a974c7bcc8e.s1.eu.hivemq.cloud"
+#define SECRET_MQTT_USER "Kababa"
+#define SECRET_MQTT_PASSWORD "Kababa wa Bongwe"
+
+#endif
